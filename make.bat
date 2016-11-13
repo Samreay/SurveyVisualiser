efd9f68234004b1e184af857889e050e390ada95
Samuel Hinton <samuelreay@gmail.com>
2016-11-14 09:56:52 +1000

    Better flags

--- make.bat
+++ make.bat
@@ -1,2 +1,2 @@
-ffmpeg -i output/%1/3d_%%d.png -c:v libx264 -preset slow -pix_fmt yuv420p -crf 26 output/%1.mp4
+ffmpeg -i output/%1/3d_%%d.png -c:v libx264 -preset veryslow -pix_fmt yuv420p -crf 27 -profile:v main output/%1.mp4
 
